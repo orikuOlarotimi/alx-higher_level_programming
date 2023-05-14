ce752e9632acd6b6de487bbc60ee85e3e4329b82
orikuOlarotimi <rotimipraise07@gmail.com>
2023-05-14 09:03:02 -0700

    Python - Data Structures: Lists, Tuples and operations involved with them

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    count = len(my_list) - 1
+    if idx > count:
+        return None
+    elif idx < 0:
+        return None
+    else:
+        print(my_list[idx])
