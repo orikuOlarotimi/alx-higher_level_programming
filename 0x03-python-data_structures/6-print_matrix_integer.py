e3605992c33b86a5b9b1a7265d8bd046e123cc73
orikuOlarotimi <rotimipraise07@gmail.com>
2023-05-15 12:36:10 -0700

    Python - Data Structures: Lists, Tuples and operations involved with them

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for k in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][k]), end="")
+            if k != (len(matrix[i]) - 1):
+                print(" ", end="")
+    print("")
