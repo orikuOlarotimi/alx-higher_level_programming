b0cef7fe7757bf33ab3030add74c33812c3e45f6
orikuOlarotimi <rotimipraise07@gmail.com>
2023-06-05 17:04:03 -0700

    a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -67,29 +67,3 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    @height.setter
-    def height(self, value):
-        """setter for the private instance attribute height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """returns the perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
-
-    def __str__(self):
-        """returns printable string representation of the rectangle"""
-        result = ""
-        if self.__width != 0 and self.__height != 0:
-            result += "\n".join("#" * self.__width for j in range(self.__height))
-        return result
