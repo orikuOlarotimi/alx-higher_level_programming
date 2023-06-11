dfaa6c53e15be5084f8040b24375b1033ab10103
orikuOlarotimi <rotimipraise07@gmail.com>
2023-06-11 09:45:43 -0700

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines an inherited list class MyList."""
+
+
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def print_sorted(self):
+         """sort items"""
+         arr = list(self)
+         arr.sort()
+         print(arr)
