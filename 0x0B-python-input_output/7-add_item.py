85f3b1765a15f375ff23db66369376651e64fb31
orikuOlarotimi <rotimipraise07@gmail.com>
2023-06-14 09:06:07 -0700

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
-"""import system"""
-from sys import argv
-"""import save from json"""
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-"""import load from json"""
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+    try:
+        item = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")
