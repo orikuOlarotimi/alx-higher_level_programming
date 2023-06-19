aa6a867bbdab7c2a9632ce3b608764c294d456a5
orikuOlarotimi <rotimipraise07@gmail.com>
2023-06-19 00:29:15 -0700

    all tasks concerned with  Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Represents the base class model """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of a new Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON serialization of a list of dicts."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the deserialization of a JSON string.
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
